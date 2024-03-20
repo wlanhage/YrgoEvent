6e65ff120455ae80e6fcbaee435ea2484d4c58dc
hugosandsjo <hugosandsjo@gmail.com>
2024-03-20 11:12:21 +0100

    didnt get .env to work but going to fix that later

--- server/migrations/database.js
+++ server/migrations/database.js
@@ -1,23 +1,41 @@
-import { createPool } from "mysql2/promise";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
-async function main() {
-  const pool = createPool({
+const pool = mysql
+  .createPool({
     host: "127.0.0.1",
     user: "root",
     password: "",
-  });
+    database: "yrgoeventDB",
+  })
+  .promise();
 
-  try {
-    const connection = await pool.getConnection();
-    console.log("Connected!");
+// skipping .env at the moment
+// const pool = mysql
+//   .createPool({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE,
+//   })
+//   .promise();
 
-    const result = await connection.query("CREATE DATABASE yrgoeventDB");
-    console.log("Database created");
+async function getStudents() {
+  const [rows] = await pool.query("SELECT * from students");
+  return rows;
+}
+const students = await getStudents();
+console.log(students);
 
-    connection.release();
-  } catch (err) {
-    console.error(err);
-  }
+async function getStudent() {
+  const [rows] = await pool.query(`
+  SELECT *
+  FROM students
+  WHERE students.name = 'John Doe'
+  `);
+  return rows[0];
 }
 
-main();
+const john = await getStudent();
+console.log(john);
